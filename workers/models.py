169edef008a95da5ad6d26708f0a08997df69345
wiktorks <wiktorks1994@gmail.com>
2022-05-02 17:05:46 +0200

    Add worker and task db model

---
+++ workers/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Worker(models.Model):
+    name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    position = models.CharField(max_length=200)
+    month_salary = models.DecimalField(max_digits=8, decimal_places=2)
+
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    status = models.CharField(max_length=200)
+    category = models.CharField(max_length=200)
+    planned_end_date = models.DateField()
+    assigned_worker = models.ForeignKey(Worker, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
