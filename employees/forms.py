b479aad8d445fff93c3921062e74816f90904907
wiktorks <wiktorks1994@gmail.com>
2022-05-04 10:53:27 +0200

    Create task form

--- employees/forms.py
+++ employees/forms.py
@@ -1,8 +1,14 @@
 from django.forms import ModelForm
-from .models import Employee
+from .models import Employee, Task
 
 
 class EmployeeForm(ModelForm):
     class Meta:
         model = Employee
         fields = ["name", "surname", "position", "month_salary"]
+
+
+class TaskForm(ModelForm):
+    class Meta:
+        model = Task
+        fields = ["title", "description", "status", "category", "planned_end_date"]
