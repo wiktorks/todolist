d27b08b4173b539ec2e629fd15a1151733394771
wiktorks <wiktorks1994@gmail.com>
2022-05-05 17:32:41 +0200

    Add mark as complete front end functionality

--- employees/static/employees/js/main.js
+++ employees/static/employees/js/main.js
@@ -2,4 +2,22 @@ $(document).ready(function () {
   $(".clickable-row").click(function () {
     window.location = $(this).data("href");
   });
+
+  $(".task-completed").on("change", function () {
+    const task_id = this.id;
+    $.ajax({
+      headers: {
+        "content-type": "application/json",
+      },
+      url: `http://localhost:8000/api/v1.0/tasks/${task_id}/update/`,
+      type: "PATCH",
+      data: JSON.stringify({ is_completed: !this.checked }),
+      success: function (response, textStatus, jqXhr) {
+        console.log("Udało się!!!");
+      },
+      error: function (response, textStatus, jqXhr) {
+        console.log("Nie udało się...");
+      },
+    });
+  });
 });
