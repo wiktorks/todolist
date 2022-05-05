f88b92cc89d16ceeecea427ad5acbdf53fc79e38
wiktorks <wiktorks1994@gmail.com>
2022-05-05 17:33:05 +0200

    Set CORS policy

--- to_do_list/settings.py
+++ to_do_list/settings.py
@@ -37,12 +37,16 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "corsheaders",
     "rest_framework",
     "crispy_forms",
     "employees",
+    "api",
 ]
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -126,3 +130,5 @@ STATIC_URL = "static/"
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
+
+CORS_ORIGIN_ALLOW_ALL = True
