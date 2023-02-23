020d82e0c2195e74ff18870c06a7064ba3f230ae
wiktorks <wiktorks1994@gmail.com>
2023-02-23 12:25:02 +0100

    Refactor project structure, add nginx as reverse proxy

--- django/employees/urls.py
+++ django/employees/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from employees import views
 
+
 urlpatterns = [
     path("", views.employee_list, name="employee-list"),
     path("employee/create/", views.employee_create, name="employee-create"),
