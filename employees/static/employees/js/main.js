f36e2e43c2ac6fec623f7683b75e8f14bfdea6ba
Karolina <karolina.konopka@sciekipolskie.org>
2023-02-23 09:12:44 +0100

    Add ajax to modal forms

--- employees/static/employees/js/main.js
+++ employees/static/employees/js/main.js
@@ -1,28 +1,28 @@
 $(document).ready(function () {
-  const taskFullList = get_task_data();
+    const taskFullList = get_task_data();
 
-  function get_task_data() {
-    let task_list = [];
-    const task_table = $(".task");
-    for (row of task_table) {
-      task_list.push({
-        is_checked: $(row).find(".task-completed-checkbox")[0].checked,
-        description: $(row).find(".task-description")[0].innerText,
-        status: $(row).find(".task-status")[0].innerText,
-        category: $(row).find(".task-category")[0].innerText,
-        dueDate: $(row).find(".task-end-date")[0].innerText,
-        taskId: $(row).find(".task-completed-checkbox")[0].id,
-        counter: $(".delete-confirm")[0].id,
-      });
+    function get_task_data() {
+        let task_list = [];
+        const task_table = $(".task");
+        for (row of task_table) {
+            task_list.push({
+                is_checked: $(row).find(".task-completed-checkbox")[0].checked,
+                description: $(row).find(".task-description")[0].innerText,
+                status: $(row).find(".task-status")[0].innerText,
+                category: $(row).find(".task-category")[0].innerText,
+                dueDate: $(row).find(".task-end-date")[0].innerText,
+                taskId: $(row).find(".task-completed-checkbox")[0].id,
+                counter: $(".delete-confirm")[0].id,
+            });
+        }
+        return task_list;
     }
-    return task_list;
-  }
 
-  function draw_task_data(task_data) {
-    const task_table = $(".task-container").empty();
-    if (task_data) {
-      for (row of task_data) {
-        $(task_table).append(`<tr class="task">
+    function draw_task_data(task_data) {
+        const task_table = $(".task-container").empty();
+        if (task_data) {
+            for (row of task_data) {
+                $(task_table).append(`<tr class="task">
         <th scope="row">
           <input
             type="checkbox"
@@ -49,94 +49,122 @@ $(document).ready(function () {
           </button>
         </td>
       </tr>`);
-      }
-    } else {
-      $(task_table).append(`<tr>
+            }
+        } else {
+            $(task_table).append(`<tr>
       <td colspan="4">
         <p>Nie znaleziono tasków</p>
       </td>
     </tr>`);
-    }
-  }
-
-  function applyFilters() {
-    let taskSnapshot = taskFullList;
-    const dateFilter = $(".date-filter")[0];
-    const statusFilter = $(".status-filter")[0];
-
-    if ($(statusFilter).find(".status-checkbox")[0].checked) {
-      const status = $(statusFilter).find("#status-select")[0].value;
-      if (status) {
-        taskSnapshot = taskSnapshot.filter((task) => {
-          return task.status === status;
-        });
-      }
-    }
-
-    if ($(dateFilter).find(".date-checkbox")[0].checked) {
-      let startDate = $(dateFilter).find("input[name='from-day']")[0].value;
-      let endDate = $(dateFilter).find("input[name='to-day']")[0].value;
-      // startDate = new Date(startDate.split("-"));
-      // endDate = new Date(endDate.split("-"));
-      // startDate.setHours(0, 0, 0, 0);
-      // endDate.setHours(0, 0, 0, 0);
-      // console.log(
-      //   `Dates filter: ${startDate.getTime()} - ${endDate.getTime()}, ${
-      //     startDate.getTime() === endDate.getTime()
-      //   }`
-      // );
-      // console.log(`Task Date: ${new Date(taskSnapshot[0].dueDate.split("-"))}`);
-      if (startDate && endDate) {
-        startDate = new Date(startDate.split("-"));
-        endDate = new Date(endDate.split("-"));
-        startDate.setHours(0, 0, 0, 0);
-        endDate.setHours(0, 0, 0, 0);
-        if (startDate.getTime() > endDate.getTime()) {
-          startDate = endDate;
         }
-        taskSnapshot = taskSnapshot.filter((task) => {
-          let task_date = new Date(task.dueDate.split("-"));
-          task_date.setHours(0, 0, 0, 0);
-          console.log(
-            task_date.getTime(),
-            startDate.getTime(),
-            endDate.getTime()
-          );
-          return (
-            task_date.getTime() >= startDate.getTime() &&
-            task_date.getTime() <= endDate.getTime()
-          );
-        });
-        console.log(taskSnapshot);
-      }
     }
 
-    draw_task_data(taskSnapshot);
-  }
+    function applyFilters() {
+        let taskSnapshot = taskFullList;
+        const dateFilter = $(".date-filter")[0];
+        const statusFilter = $(".status-filter")[0];
 
-  $(".filter-checkbox").on("change", applyFilters);
-  $("#status-select").on("change", applyFilters);
-  $("input[type='date']").on("change", applyFilters);
+        if ($(statusFilter).find(".status-checkbox")[0].checked) {
+            const status = $(statusFilter).find("#status-select")[0].value;
+            if (status) {
+                taskSnapshot = taskSnapshot.filter((task) => {
+                    return task.status === status;
+                });
+            }
+        }
 
-  $(".clickable-row").click(function () {
-    window.location = $(this).data("href");
-  });
+        if ($(dateFilter).find(".date-checkbox")[0].checked) {
+            let startDate = $(dateFilter).find("input[name='from-day']")[0].value;
+            let endDate = $(dateFilter).find("input[name='to-day']")[0].value;
+            // startDate = new Date(startDate.split("-"));
+            // endDate = new Date(endDate.split("-"));
+            // startDate.setHours(0, 0, 0, 0);
+            // endDate.setHours(0, 0, 0, 0);
+            // console.log(
+            //   `Dates filter: ${startDate.getTime()} - ${endDate.getTime()}, ${
+            //     startDate.getTime() === endDate.getTime()
+            //   }`
+            // );
+            // console.log(`Task Date: ${new Date(taskSnapshot[0].dueDate.split("-"))}`);
+            if (startDate && endDate) {
+                startDate = new Date(startDate.split("-"));
+                endDate = new Date(endDate.split("-"));
+                startDate.setHours(0, 0, 0, 0);
+                endDate.setHours(0, 0, 0, 0);
+                if (startDate.getTime() > endDate.getTime()) {
+                    startDate = endDate;
+                }
+                taskSnapshot = taskSnapshot.filter((task) => {
+                    let task_date = new Date(task.dueDate.split("-"));
+                    task_date.setHours(0, 0, 0, 0);
+                    console.log(
+                        task_date.getTime(),
+                        startDate.getTime(),
+                        endDate.getTime()
+                    );
+                    return (
+                        task_date.getTime() >= startDate.getTime() &&
+                        task_date.getTime() <= endDate.getTime()
+                    );
+                });
+                console.log(taskSnapshot);
+            }
+        }
 
-  $(".task-completed").on("change", function () {
-    const task_id = this.id;
-    $.ajax({
-      headers: {
-        "content-type": "application/json",
-      },
-      url: `http://localhost:8000/api/v1.0/tasks/${task_id}/update/`,
-      type: "PATCH",
-      data: JSON.stringify({ is_completed: this.checked }),
-      success: function (response, textStatus, jqXhr) {
-        console.log("Udało się!!!");
-      },
-      error: function (response, textStatus, jqXhr) {
-        console.log("Nie udało się...");
-      },
+        draw_task_data(taskSnapshot);
+    }
+
+    $(".filter-checkbox").on("change", applyFilters);
+    $("#status-select").on("change", applyFilters);
+    $("input[type='date']").on("change", applyFilters);
+
+    $(".clickable-row").click(function () {
+        window.location = $(this).data("href");
     });
-  });
+
+    $(".task-completed").on("change", function () {
+        const task_id = this.id;
+        $.ajax({
+            headers: {
+                "content-type": "application/json",
+            },
+            url: `http://localhost:8000/api/v1.0/tasks/${task_id}/update/`,
+            type: "PATCH",
+            data: JSON.stringify({is_completed: this.checked}),
+            success: function (response, textStatus, jqXhr) {
+                console.log("Udało się!!!");
+            },
+            error: function (response, textStatus, jqXhr) {
+                console.log("Nie udało się...");
+            },
+        });
+    });
+
+    function prepareModalForm() {
+        console.log("prepareModalForm");
+        $('#modalForm').submit(function (e) {
+            e.preventDefault();
+            var modal = $('#createModal')
+            $.ajax({
+                url: $('#modalForm').attr('action'),
+                type: 'POST',
+                data: $('#modalForm').serialize(),
+                success: function (xhr, ajaxOptions, thrownError) {
+
+                    $(modal).find('.modal-body').html(xhr);
+
+                    if ($(xhr).find('.invalid-feedback, .errorlist').length === 0) {
+                        {
+                            $(modal).modal('hide');
+                            window.location.reload(true);
+                        }
+                    } else {
+                        prepareModalForm()
+                    }
+                }
+            });
+            return false;
+        });
+    }
+    prepareModalForm();
 });
